282f943cfdbcd2d4afdc8318cbba802ce10d3b8d
Daniel Silva <danielsilva@bossabox.com>
2022-05-06 09:45:29 -0300

    feat: initial commit

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,14 @@
 <script>
-	export let name;
+  import EventComponent from "./EventComponent.svelte";
+
+  function handleMessage(event) {
+    alert(event.detail.text);
+  }
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <p>Event communication.</p>
+  <hr />
+
+  <EventComponent on:message={handleMessage} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
